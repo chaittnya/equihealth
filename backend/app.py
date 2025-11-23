ba6b662db587d85a3ee0affd9582660f4710c548
Chaitanya Shinde <chaittnyashinde@gmail.com>
2025-11-23 22:13:08 +0530

    corrected cors origin

--- backend/app.py
+++ backend/app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "https://equihealth-1.onrender.com/"
+    response.headers["Access-Control-Allow-Origin"] = "https://equihealth-1.onrender.com"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
@@ -25,7 +25,7 @@ def add_cors_headers(response):
 
 CORS(app, resources={
     r"/api/*": {
-        "origins": "https://equihealth-1.onrender.com/"
+        "origins": "https://equihealth-1.onrender.com"
     }
 })
 
